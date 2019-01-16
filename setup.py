635be618d9a3a3b59757d85a8e35f8c6cf1236c9
Jason Carver <ut96caarrs@snkmail.com>
2019-01-15 16:06:18 -0800

    Drop py3.5, add py3.7

--- setup.py
+++ setup.py
@@ -80,8 +80,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
 )
