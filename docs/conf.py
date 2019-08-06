74621d40e80ff1cb9305dc6e0372b7b8681c8f38
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2019-08-06 10:46:27 +0200

    Fix wrong title in docs and license

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# <PROJECT_NAME> documentation build configuration file, created by
+# lahja documentation build configuration file, created by
 # sphinx-quickstart on Thu Oct 16 20:43:24 2014.
 #
 # This file is execfile()d with the current directory set to its
@@ -55,8 +55,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = '<PROJECT_NAME>'
-copyright = '2018, Jason Carver, Piper Merriam'
+project = 'lahja'
+copyright = '2018-2019 Ethereum Foundation'
 
 __version__ = setup_version
 # The version info for the project you're documenting, acts as replacement for
@@ -214,8 +214,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'lahja.tex', '<PROJECT_NAME> Documentation',
-   'Jason Carver', 'manual'),
+  ('index', 'lahja.tex', 'lahja Documentation', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -244,8 +243,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'lahja', '<PROJECT_NAME> Documentation',
-     ['Jason Carver'], 1)
+    ('index', 'lahja', 'lahja Documentation', 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -258,8 +256,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', '<PROJECT_NAME>', '<PROJECT_NAME> Documentation',
-   'Jason Carver', '<PROJECT_NAME>', '<SHORT_DESCRIPTION>',
+  ('index', 'lahja', 'lahja Documentation', 'lahja',
+  'Lahja is a generic multi process event bus',
    'Miscellaneous'),
 ]
 
