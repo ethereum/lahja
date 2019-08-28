a19de7209f1114b2e3cef0f2802ac3133f1205ef
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2019-08-28 10:26:10 +0200

    Fix broken Latex generation

--- docs/conf.py
+++ docs/conf.py
@@ -214,7 +214,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'lahja.tex', 'lahja Documentation', 'manual'),
+  ('index', 'lahja.tex', 'lahja Documentation', 'Ethereum Foundation', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -243,7 +243,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'lahja', 'lahja Documentation', 1)
+    ('index', 'lahja', 'lahja Documentation', ['Ethereum Foundation'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -256,7 +256,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'lahja', 'lahja Documentation', 'lahja',
+  ('index', 'lahja', 'lahja Documentation', 'Ethereum Foundation', 'lahja',
   'Lahja is a generic multi process event bus',
    'Miscellaneous'),
 ]
